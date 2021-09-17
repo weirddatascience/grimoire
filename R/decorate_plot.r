5ab8bb310463339c3ae380e635f6f4d4a1b3e976
Joss Wright <joss@pseudonymity.net>
2021-09-17 12:22:49 +0100

    Minor updates to decorate_plot function.

--- R/decorate_plot.r
+++ R/decorate_plot.r
@@ -6,6 +6,7 @@
 #' @param subtitle Subtitle text for the plot.
 #' @param footer Footer text, for references and acknowledgements.
 #' @param plot ggplot plot object to centralize.	
+#' @param bg_colour Background colour for the plot. If given, this will override the main plot's background by setting it to be transparent, and cause this colour to span the entire plot without borders. Note that this argument will override any \code{plot.background} or \code{panel.background} settings in the \code{decorate_theme} argument.
 #' @param bg_image Path to a background image for the page. This will span the entire plot without borders.
 #' @param theme A theme object to use in constructing the titles and footer.
 #' @param rel_heights Array of relative heights for the title, plot, and footer sections. Defaults to c(0.1, 1, 0.05).
@@ -14,45 +15,81 @@
 #' @examples
 #' df <- data.frame( x=rnorm(100), y=rnorm(100) )
 #' gp <- ggplot( df, aes(x=x, y=y) ) + geom_point()
-#' dp <- decorate_plot( "Plot Title", "Weird Data Science", gp, "Data: Normal Distribution" )
+#' dp <- decorate_plot( gp, "Plot Title", "Weird Data Science", "Data: Normal Distribution" )
  
-decorate_plot <- function( title = NULL, 
+decorate_plot <- function( plot, 
+								  	title = NULL, 
 								  	subtitle="http://www.weirddatascience.net | @WeirdDataSci", 
 									footer = NULL,
-									plot, 
+									bg_colour = NULL,
 									bg_image = NULL,
 									decorate_theme = NULL,
 									rel_heights = c( 0.1, 1, 0.05 ) ) {
+
+	bg_theme <- NULL
+	# Add the plot background and panel background if bg_colour is given
+	if( !is.null( bg_colour ) ) {
+
+		bg_theme <-
+			theme(	plot.background = element_rect( colour="transparent", fill=bg_colour ),
+						panel.background = element_rect( colour="transparent", fill=bg_colour ) )
+
+	}
 
 	# Cowplot trick for ggtitle
 	plot_title <- 
 		cowplot::ggdraw() + 
 		cowplot::draw_label( title, 
-					  	fontfamily="bold_font", 
+					  	fontfamily="main", 
+						fontface="bold",
 						colour = "#3c3f4a", 
 						size=20, 
 						hjust=0, vjust=1, 
 						x=0.02, y=0.88) +
 		cowplot::draw_label( subtitle, 
-					  	fontfamily="bold_font", 
+					  	fontfamily="main", 
+						fontface="bold",
 						colour = "#3c3f4a", 
 						size=12, 
 						hjust=0, vjust=1, 
 						x=0.02, y=0.40) +
-		theme_decorate
+		decorate_theme +
+		bg_theme
 
 	data_label <- 
 		cowplot::ggdraw() +
 		cowplot::draw_label( footer, 
-					  	fontfamily="bold_font", 
+					  	fontfamily="main", 
 						colour = "#3c3f4a", 
 						size=8, hjust=1, x=0.98 ) +
-	theme_decorate
+		decorate_theme +
+		bg_theme
+
+	# Assert a background colour over the entire main plot, if supplied.
+	# (This is particularly useful when decorating maps made with geom_sf
+	# that assert their own aspect ratio.)
+	if( !is.null( bg_colour ) ) {
+
+		# Make the base plot background elements transparent
+		plot <- 
+			plot + 
+			theme( 	plot.background = element_blank(),
+						panel.background = element_blank() )
+
+		# Place the now-transparent plot into a themed ggdraw plot
+		plot <-
+			cowplot::ggdraw( plot ) +
+			bg_theme
+
+	}
 
 	combined_plot <- 
 		cowplot::plot_grid( 	plot_title, 
-								 	plot, data_label, 
-									ncol=1, rel_heights=rel_heights ) 
+								 plot, 
+								 data_label, 
+								 ncol=1, 
+								 rel_heights=rel_heights ) +
+	decorate_theme
 
 	# Add the background image if it has been specified.
 	if( !is.null( bg_image ) ) {
--- R/theme.r
+++ R/theme.r
@@ -2,20 +2,20 @@
 
 #' @export
 theme_weird <- 
-	function(base_size = 12, base_family = "main_font") {
+	function(base_size = 12, base_family = "main") {
 		ggplot2::theme_dark(base_size = base_size, base_family = base_family) %+replace%
 		ggplot2::theme( 
 				panel.background = ggplot2::element_rect(fill = "#222222", colour = "#222222"),
 				plot.background = ggplot2::element_rect(fill = "#222222", colour = "#222222"),
-				plot.title = ggplot2::element_text( size=16, color="#eeeeee", family="bold_font" ),
-				plot.subtitle = ggplot2::element_text( size=14, color="#eeeeee", family="main_font" ),
+				plot.title = ggplot2::element_text( size=16, color="#eeeeee", family="main", face="bold" ),
+				plot.subtitle = ggplot2::element_text( size=14, color="#eeeeee", family="main" ),
 				legend.key = ggplot2::element_rect(fill = "#222222"),
 				legend.background = ggplot2::element_rect(fill = "#222222", colour = "#222222" ),
-				legend.title = ggplot2::element_text( size=14, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				legend.text = ggplot2::element_text( size=12, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				axis.title.x = ggplot2::element_text( size=14, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				axis.title.y = ggplot2::element_text( size=14, color="#eeeeee", family="main_font", margin = ggplot2::margin( r = 20 ) ),
-				axis.text = ggplot2::element_text( size=10, color="#eeeeee", family="main_font" ),
+				legend.title = ggplot2::element_text( size=14, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				legend.text = ggplot2::element_text( size=12, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				axis.title.x = ggplot2::element_text( size=14, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				axis.title.y = ggplot2::element_text( size=14, color="#eeeeee", family="main", margin = ggplot2::margin( r = 20 ) ),
+				axis.text = ggplot2::element_text( size=10, color="#eeeeee", family="main" ),
 				panel.grid.major.x = ggplot2::element_blank(),
 				panel.grid.major.y = ggplot2::element_line(colour = "#444444"),
 				panel.grid.minor.x = ggplot2::element_blank(),
@@ -25,18 +25,18 @@ theme_weird <-
 
 #' @export
 theme_weird_pdf <- 
-	function(base_size = 12, base_family = "main_font"){
+	function(base_size = 12, base_family = "main"){
 		ggplot2::theme_dark(base_size = base_size, base_family = base_family) %+replace%
 		ggplot2::theme( 
 				panel.background = ggplot2::element_rect(fill = "#222222", colour = "#222222"),
 				plot.background = ggplot2::element_rect(fill = "#222222", colour = "#222222"),
 				legend.key = ggplot2::element_rect(fill = "#222222"),
 				legend.background = ggplot2::element_rect(fill = "#222222", colour = "#222222" ),
-				legend.title = ggplot2::element_text( size=10, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				legend.text = ggplot2::element_text( size=8, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				axis.title.x = ggplot2::element_text( size=10, color="#eeeeee", family="main_font", margin = ggplot2::margin( t = 20 ) ),
-				axis.title.y = ggplot2::element_text( size=10, color="#eeeeee", family="main_font", margin = ggplot2::margin( r = 20 ) ),
-				axis.text = ggplot2::element_text( size=6, color="#eeeeee", family="main_font" ),
+				legend.title = ggplot2::element_text( size=10, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				legend.text = ggplot2::element_text( size=8, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				axis.title.x = ggplot2::element_text( size=10, color="#eeeeee", family="main", margin = ggplot2::margin( t = 20 ) ),
+				axis.title.y = ggplot2::element_text( size=10, color="#eeeeee", family="main", margin = ggplot2::margin( r = 20 ) ),
+				axis.text = ggplot2::element_text( size=6, color="#eeeeee", family="main" ),
 				panel.grid.major = ggplot2::element_line(colour = "#444444"),
 				panel.grid.minor = ggplot2::element_line(colour = "#444444"),
 				)
